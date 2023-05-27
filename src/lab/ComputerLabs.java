bd7d83430027708f74f62e01639d73722d1dace4
CartoonChess <philip@hkpp.fyi>
2023-05-27 14:19:11 +0900

    Created 3D array and lab status table

--- src/lab/ComputerLabs.java
+++ src/lab/ComputerLabs.java
@@ -2,6 +2,24 @@ package lab;
 
 public class ComputerLabs {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String empty = "_____";
+
+        String[][] labsAndStations = {
+            {empty, empty, empty, empty, empty},
+            {empty, empty, empty, empty, empty, empty},
+            {empty, empty, empty, empty},
+            {empty, empty, empty},
+        };
+
+        System.out.println("");
+        System.out.println("LAB STATUS");
+        System.out.println("Lab #\tComputer Stations");
+        for (int labNo = 0; labNo < labsAndStations.length; labNo++) {
+            System.out.print(labNo + 1 + "\t");
+            for (int stationNo = 0; stationNo < labsAndStations[labNo].length; stationNo++) {
+                System.out.print(stationNo + 1 + ": " + labsAndStations[labNo][stationNo] + "  ");
+            }
+            System.out.println("");
+        }
     }
 }
